fd8cdf572d6a589ac3a625d44bbad62fd55d6f6d
Larbi Nesrine (External) <Nesrine.Larbi.Guest@saipem.com>
2025-06-05 10:47:45 +0200

    add some media for my pages and an undebugged version of portfolio

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import projetsData from '../data/projets.json';
+import Projet from '../Components/Projet';
 
 const Portfolio = () => {
-    return (
-        <div>
-            <h1>ici c est portfolio</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Mes projets</h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+        {projetsData.map((projet, index) => {
+          const imagePath = require(`../media/${projet.image}`);
+          return <Projet key={index} projet={projet} image={imagePath} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
